61b8dcf5ef7cee2ed0dc2aae1ed2fad797768067
ghochart <gabriel.hochart@laposte.net>
2014-09-18 15:23:30 +0200

    Update ft_putnbr.c

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -9,18 +9,22 @@
 /*   Updated: 2014/09/09 17:50:54 by ghochart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-void    ft_putchar(char c)
-{
-  write(1, &c, 1);
-}
-
 void    ft_putnbr(int n)
 {
-  if (n >= 10)
-    {
-      ft_putnbr(n / 10);
-      ft_putnbr(n % 10);
-    }
+  if (nb < 0)
+  {
+    ft_putchar('-');
+    nb = -nb;
+  }
+  if (n > 10)
+  {
+    ft_putnbr(n / 10);
+    ft_putnbr(n % 10);
+  }
+  else if (n < 0)
+  {
+    
+  }
   else
     ft_putchar(48 + n);
 }
